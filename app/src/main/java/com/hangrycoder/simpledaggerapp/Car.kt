5ae776abc24e3d44acd383c8c6b3d99469dd9e7b
HangrySonia <sonia.wadji@unacademy.com>
2021-04-29 17:18:54 +0530

    Created dummy car, engine and wheels classes

---
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/Car.kt
@@ -0,0 +1,11 @@
+package com.hangrycoder.simpledaggerapp
+
+class Car {
+
+    val engine: Engine = Engine()
+    val wheels: Wheels = Wheels()
+
+    fun drive() {
+
+    }
+}
---
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/Engine.kt
@@ -0,0 +1,4 @@
+package com.hangrycoder.simpledaggerapp
+
+class Engine {
+}
