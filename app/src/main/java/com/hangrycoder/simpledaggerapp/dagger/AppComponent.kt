1384ce53f1059a3b827c33ceb1c8350011346bf4
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 19:58:46 +0530

    Created an AppComponent interface to getDriver

--- app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/AppComponent.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/AppComponent.kt
@@ -1,8 +1,11 @@
 package com.hangrycoder.simpledaggerapp.dagger
 
+import com.hangrycoder.simpledaggerapp.car.Driver
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component
-interface AppComponent
+interface AppComponent {
+    fun getDriver(): Driver
+}
