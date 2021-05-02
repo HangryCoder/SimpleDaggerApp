03e753e66dbfdce0bc10abbd049a48af3c5e7b9c
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 18:33:58 +0530

    Added another constructor param called engineCapacity.

--- app/src/main/java/com/hangrycoder/simpledaggerapp/car/PetrolEngine.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/car/PetrolEngine.kt
@@ -1,14 +1,16 @@
 package com.hangrycoder.simpledaggerapp.car
 
 import android.util.Log
-import com.hangrycoder.simpledaggerapp.car.Engine
 import javax.inject.Inject
 
-class PetrolEngine @Inject constructor(private val horsePower: Int) :
+class PetrolEngine @Inject constructor(
+    private val horsePower: Int,
+    private val engineCapacity: Int
+) :
     Engine {
     private val TAG = "Car"
 
     override fun start() {
-        Log.d(TAG, "Petrol Engine started horsePower: $horsePower")
+        Log.d(TAG, "Petrol Engine started horsePower: $horsePower engineCapacity: $engineCapacity")
     }
 }
