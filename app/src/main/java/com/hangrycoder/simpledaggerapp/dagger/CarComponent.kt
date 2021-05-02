bcf09a7f835654a260dffca0b9485ae3e1172be9
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 18:29:44 +0530

    Creating a custom builder of my own to pass horsePower directly into the build method

--- app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/CarComponent.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/CarComponent.kt
@@ -2,11 +2,20 @@ package com.hangrycoder.simpledaggerapp.dagger
 
 import com.hangrycoder.simpledaggerapp.MainActivity
 import com.hangrycoder.simpledaggerapp.car.Car
+import dagger.BindsInstance
 import dagger.Component
 
-@Component(modules = [WheelsModule::class, DieselEngineModule::class])
+@Component(modules = [WheelsModule::class, PetrolEngineModule::class])
 interface CarComponent {
     fun getCar(): Car
 
     fun inject(activity: MainActivity)
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun horsePower(horsePower: Int): Builder
+        fun build(): CarComponent
+    }
 }
