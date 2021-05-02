77f2b0e75a304e21b96cad7777b9b6b71f6655bc
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 18:11:55 +0530

    Converted binds engine function to provides method. Created dieselEngine at runtime with horsePower

--- app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/DieselEngineModule.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/DieselEngineModule.kt
@@ -2,12 +2,15 @@ package com.hangrycoder.simpledaggerapp.dagger
 
 import com.hangrycoder.simpledaggerapp.car.DieselEngine
 import com.hangrycoder.simpledaggerapp.car.Engine
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import javax.inject.Inject
 
 @Module
-abstract class DieselEngineModule {
+class DieselEngineModule @Inject constructor(private val horsePower: Int) {
 
-    @Binds
-    abstract fun bindEngine(engine: DieselEngine): Engine
+    @Provides
+    fun providesEngine(): Engine {
+        return DieselEngine(horsePower)
+    }
 }
