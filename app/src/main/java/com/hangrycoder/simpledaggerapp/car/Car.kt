2ef983ad007ee6eadacd047d399bad607680546a
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 19:56:16 +0530

    Ddded PerActivity scope to it

--- app/src/main/java/com/hangrycoder/simpledaggerapp/car/Car.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/car/Car.kt
@@ -1,8 +1,10 @@
 package com.hangrycoder.simpledaggerapp.car
 
 import android.util.Log
+import com.hangrycoder.simpledaggerapp.dagger.PerActivity
 import javax.inject.Inject
 
+@PerActivity
 class Car @Inject constructor(val engine: Engine, val wheels: Wheels, val driver: Driver) {
 
     private val TAG by lazy {
