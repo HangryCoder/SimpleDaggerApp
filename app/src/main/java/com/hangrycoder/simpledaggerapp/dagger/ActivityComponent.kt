1accbcc303c94198aea76e003974d12b0522b051
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 22:21:32 +0530

    Changed ActivityComponent.kt to Subcomponent. Added a function getActivity component in place of getDriver in AppComponent

--- app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/ActivityComponent.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/ActivityComponent.kt
@@ -4,29 +4,29 @@ import com.hangrycoder.simpledaggerapp.MainActivity
 import com.hangrycoder.simpledaggerapp.car.Car
 import dagger.BindsInstance
 import dagger.Component
+import dagger.Subcomponent
 import javax.inject.Named
 
 @PerActivity
-@Component(
-    dependencies = [AppComponent::class],
-    modules = [WheelsModule::class, PetrolEngineModule::class]
+@Subcomponent(
+    modules = [WheelsModule::class, DieselEngineModule::class]
 )
 interface ActivityComponent {
     fun getCar(): Car
 
     fun inject(activity: MainActivity)
 
-    @Component.Builder
-    interface Builder {
+    /* @Component.Builder
+     interface Builder {
 
-        @BindsInstance
-        fun horsePower(@Named("horsePower") horsePower: Int): Builder
+         @BindsInstance
+         fun horsePower(@Named("horsePower") horsePower: Int): Builder
 
-        @BindsInstance
-        fun engineCapacity(@Named("engineCapacity") engineCapacity: Int): Builder
+         @BindsInstance
+         fun engineCapacity(@Named("engineCapacity") engineCapacity: Int): Builder
 
-        fun appComponent(component: AppComponent): Builder
+         fun appComponent(component: AppComponent): Builder
 
-        fun build(): ActivityComponent
-    }
+         fun build(): ActivityComponent
+     }*/
 }
--- app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/AppComponent.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/AppComponent.kt
@@ -1,12 +1,10 @@
 package com.hangrycoder.simpledaggerapp.dagger
 
-import com.hangrycoder.simpledaggerapp.car.Driver
 import dagger.Component
-import dagger.Module
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [DriverModule::class])
 interface AppComponent {
-    fun getDriver(): Driver
+    fun getActivityComponent(): ActivityComponent
 }
