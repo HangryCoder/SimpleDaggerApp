3a5ffaa022ef72daa9f28d85bee447d38d79430d
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 19:36:01 +0530

    Added car Component to Application class. Also added it in Manifest

--- app/src/main/java/com/hangrycoder/simpledaggerapp/SimpleDaggerApp.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/SimpleDaggerApp.kt
@@ -1,10 +1,21 @@
 package com.hangrycoder.simpledaggerapp
 
 import android.app.Application
+import com.hangrycoder.simpledaggerapp.dagger.CarComponent
+import com.hangrycoder.simpledaggerapp.dagger.DaggerCarComponent
 
 class SimpleDaggerApp : Application() {
 
+    private lateinit var component: CarComponent
+
     override fun onCreate() {
         super.onCreate()
+
+        component = DaggerCarComponent.builder()
+            .horsePower(120)
+            .engineCapacity(1400)
+            .build()
     }
+
+    fun getAppComponent() = component
 }
