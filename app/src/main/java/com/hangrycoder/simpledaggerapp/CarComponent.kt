26a5d74a1f5f7aa1ceb2e83b810f97e226a00fcd
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 17:52:50 +0530

    Added DieselEngineModule to Car component. Car is using diesel engine now

--- app/src/main/java/com/hangrycoder/simpledaggerapp/CarComponent.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/CarComponent.kt
@@ -2,7 +2,7 @@ package com.hangrycoder.simpledaggerapp
 
 import dagger.Component
 
-@Component(modules = [WheelsModule::class, PetrolEngineModule::class])
+@Component(modules = [WheelsModule::class, DieselEngineModule::class])
 interface CarComponent {
     fun getCar(): Car
 
