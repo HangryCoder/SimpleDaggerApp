71d24280df56dd87eb80c0962f639bebf5d3b0c2
HangrySonia <sonia.wadji@unacademy.com>
2021-05-02 20:02:03 +0530

    Added the Driver Module to AppComponent.kt

--- app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/AppComponent.kt
+++ app/src/main/java/com/hangrycoder/simpledaggerapp/dagger/AppComponent.kt
@@ -2,10 +2,12 @@ package com.hangrycoder.simpledaggerapp.dagger
 
 import com.hangrycoder.simpledaggerapp.car.Driver
 import dagger.Component
+import dagger.Module
 import javax.inject.Singleton
 
 @Singleton
 @Component
+@Module(includes = [DriverModule::class])
 interface AppComponent {
     fun getDriver(): Driver
 }
